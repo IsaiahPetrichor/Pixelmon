52795e2e7c1ff23f715f4d7fa747a27b2ef0af18
Isaiah Petrichor <petrichorwebdev@gmail.com>
2026-09-07 15:57:59 -0700

    added initial population file, and inserted values into local db

---
+++ Pixelmon.Database/InitialPopulation.sql
@@ -0,0 +1,76 @@
+INSERT INTO regions (region_name)
+VALUES ('Kanto'), ('Hoenn'), ('Sinnoh');
+
+INSERT INTO staffranks (rank_name, permission_level)
+VALUES ('Owner', 1), ('Admin', 2), ('Moderator', 3);
+
+INSERT INTO staff (username, rank_id)
+VALUES ('Fallen', 1), ('Zyrex', 1);
+
+INSERT INTO workareas (area_name)
+VALUES ('Terrain'),
+    ('Buildings'),
+    ('Foliage'),
+    ('Pokemon'),
+    ('StoryNPCs'),
+    ('ShopNPCs'),
+    ('ChattingNPCs'),
+    ('Trainers'),
+    ('Items');
+
+INSERT INTO workitemstatuses (status_name)
+VALUES ('New'), ('In Progress'), ('Complete');
+
+INSERT INTO routes (region_id, route_name)
+VALUES (1, 'Route 1'),
+    (1, 'Route 2'),
+    (1, 'Route 3'),
+    (1, 'Route 4'),
+    (1, 'Route 5'),
+    (1, 'Route 6'),
+    (1, 'Route 7'),
+    (1, 'Route 8'),
+    (1, 'Route 9'),
+    (1, 'Route 10'),
+    (1, 'Route 11'),
+    (1, 'Route 12'),
+    (1, 'Route 13'),
+    (1, 'Route 14'),
+    (1, 'Route 15'),
+    (1, 'Route 16'),
+    (1, 'Route 17'),
+    (1, 'Route 18'),
+    (1, 'Route 19'),
+    (1, 'Route 20'),
+    (1, 'Route 21'),
+    (1, 'Route 22'),
+    (1, 'Route 23'),
+    (1, 'Route 24'),
+    (1, 'Route 25'),
+    (1, 'Cerulean Cave'),
+    (1, 'Cerulean City'),
+    (1, 'Cinnabar Island'),
+    (1, 'Celadon City'),
+    (1, 'Diglett''s Cave'),
+    (1, 'Fuchsia City'),
+    (1, 'Indigo Plateau'),
+    (1, 'Lavender Town'),
+    (1, 'Mt. Moon'),
+    (1, 'Pallet Town'),
+    (1, 'Pewter City'),
+    (1, 'Pokemon Mansion'),
+    (1, 'Pokemon Tower'),
+    (1, 'Power Plant'),
+    (1, 'Rock Tunnel'),
+    (1, 'Rocket Hideout'),
+    (1, 'Safari Zone'),
+    (1, 'Saffron City'),
+    (1, 'Seafoam Islands'),
+    (1, 'Silph Co.'),
+    (1, 'SS Anne'),
+    (1, 'Underground Path 5-6'),
+    (1, 'Underground Path 7-8'),
+    (1, 'Vermilion City'),
+    (1, 'Victory Road'),
+    (1, 'Viridian City'),
+    (1, 'Viridian Forest');
